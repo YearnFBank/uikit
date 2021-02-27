9766ed523f02a71d1cd1e27aa7046d2c769f5314
Romuald <romuald.jeanjean@gmail.com>
2021-02-27 13:43:52 +0100

    feat: Change spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import styled  from "styled-components";
-import { CoffeeLoading } from 'react-loadingg';
+import styled from "styled-components";
+import { CoffeeLoading } from "react-loadingg";
 import { SpinnerProps } from "./types";
-import { baseColors} from "../../theme/colors";
+import { baseColors } from "../../theme/colors";
 
 const Container = styled.div`
   position: relative;
 `;
 
-const Spinner: React.FC<SpinnerProps> = ({ size = 160, color= baseColors.primary }) => {
+const Spinner: React.FC<SpinnerProps> = ({ size = 160, color = baseColors.primary }) => {
   return (
     <Container>
-        <CoffeeLoading
-            type="Rings"
-            color={color}
-            height={size}
-            width={size}
-            timeout={3000} // 3 secs
-        />
+      <CoffeeLoading
+        type="Rings"
+        color={color}
+        height={size}
+        width={size}
+        timeout={3000} // 3 secs
+      />
     </Container>
   );
 };
