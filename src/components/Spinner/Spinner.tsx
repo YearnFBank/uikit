d1a8966bd74b8a027cffe2d1aaa0a9415a8f84e6
Romuald <romuald.jeanjean@gmail.com>
2021-02-27 13:52:15 +0100

    feat: Change spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ReactLoading from 'react-loading';
+import ReactLoading from "react-loading";
 import { SpinnerProps } from "./types";
 import { baseColors } from "../../theme/colors";
 
@@ -11,12 +11,7 @@ const Container = styled.div`
 const Spinner: React.FC<SpinnerProps> = ({ size = 160, color = baseColors.primary }) => {
   return (
     <Container>
-      <ReactLoading
-      type="spin"
-        color={color}
-        height={size}
-        width={size}
-      />
+      <ReactLoading type="spin" color={color} height={size} width={size} />
     </Container>
   );
 };
