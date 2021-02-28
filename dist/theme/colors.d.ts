f2fd7344e917e3bbd5b6685bf5bccb90fdc1bfd2
Romuald <romuald.jeanjean@gmail.com>
2021-02-28 16:33:23 +0100

    feat: Build for blackswap

---
+++ dist/theme/colors.d.ts
@@ -0,0 +1,20 @@
+import { Colors } from "./types";
+export declare const baseColors: {
+    failure: string;
+    primary: string;
+    primaryBright: string;
+    primaryDark: string;
+    secondary: string;
+    success: string;
+    warning: string;
+    text: string;
+    input: string;
+    borderColor: string;
+    tertiary: string;
+    overlay: string;
+};
+export declare const brandColors: {
+    binance: string;
+};
+export declare const lightColors: Colors;
+export declare const darkColors: Colors;
